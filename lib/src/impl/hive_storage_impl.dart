dff6fecc54f9f17f5208f453cdaec547036aac51
huwentao <hu.wentao@outlook.com>
2022-02-11 22:11:33 +0800

    refactor hive_storage_impl.dart HiveStorage泛型由 IAgg改为IDto

--- lib/src/impl/hive_storage_impl.dart
+++ lib/src/impl/hive_storage_impl.dart
@@ -65,14 +65,14 @@ class HiveStorageString extends BaseHiveStorage<String, String, String>
 /// 同步box
 /// Object(IDto) -> Map<String,dynamic> -> String
 /// 使用前务必调用 [await ::preInit()]
-class HiveStorage<Ag extends IAgg>
-    extends BaseHiveStorage<Ag, Map<String, dynamic>, String>
+class HiveStorage<Dto extends IDto>
+    extends BaseHiveStorage<Dto, Map<String, dynamic>, String>
     with
-        HiveStorageSyncMx<Ag, Map<String, dynamic>, String>,
+        HiveStorageSyncMx<Dto, Map<String, dynamic>, String>,
         JsStrTRMapper,
-        DtoJsStringOTRMapper<Ag> {
+        DtoJsStringOTRMapper<Dto> {
   @override
-  final Ag Function(Map<String, dynamic> transT) cvTrans2Obj;
+  final Dto Function(Map<String, dynamic> transT) cvTrans2Obj;
 
   HiveStorage(StorageConfig config, this.cvTrans2Obj) : super(config);
 }
@@ -80,15 +80,15 @@ class HiveStorage<Ag extends IAgg>
 ///
 /// 异步box
 /// 因为也会调用同步方法,所以混入[HiveStorageSyncMx]
-class HiveAsyncStorage<Ag extends IAgg>
-    extends BaseHiveStorage<Ag, Map<String, dynamic>, String>
+class HiveAsyncStorage<Dto extends IDto>
+    extends BaseHiveStorage<Dto, Map<String, dynamic>, String>
     with
-        HiveStorageSyncMx<Ag, Map<String, dynamic>, String>,
-        HiveStorageASyncMx<Ag, Map<String, dynamic>, String>,
+        HiveStorageSyncMx<Dto, Map<String, dynamic>, String>,
+        HiveStorageASyncMx<Dto, Map<String, dynamic>, String>,
         JsStrTRMapper,
-        DtoJsStringOTRMapper<Ag> {
+        DtoJsStringOTRMapper<Dto> {
   @override
-  final Ag Function(Map<String, dynamic> transT) cvTrans2Obj;
+  final Dto Function(Map<String, dynamic> transT) cvTrans2Obj;
 
   HiveAsyncStorage(StorageConfig config, this.cvTrans2Obj) : super(config);
 }
