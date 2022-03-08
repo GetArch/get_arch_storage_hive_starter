94b8b158a0e19e1989ea3b61509a7fc60959b54f
huwentao <hu.wentao@outlook.com>
2022-03-08 14:34:20 +0800

    save

--- lib/src/base/storage_config.dart
+++ lib/src/base/storage_config.dart
@@ -2,14 +2,21 @@ import 'package:get_arch_core/get_arch_core.dart';
 
 ///
 /// 存储信息配置
-class StorageConfig extends ValueObject implements IDto {
+class StorageConfig extends BaseConfig {
   final String basePath;
   final String? key;
 
-  const StorageConfig({
-    required this.basePath,
-    this.key,
-  });
+  StorageConfig(this.basePath, this.key,
+      {required EnvSign sign,
+      required String name,
+      required String version,
+      required DateTime packAt})
+      : super(sign: sign, name: name, version: version, packAt: packAt);
+
+  // const StorageConfig({
+  //   required this.basePath,
+  //   this.key,
+  // });
 
   @override
   List<Object?> get props => [basePath, key];
--- lib/src/get_arch_storage_hive.dart
+++ lib/src/get_arch_storage_hive.dart
@@ -1,8 +1,7 @@
 import 'package:get_arch_core/get_arch_core.dart';
 import 'package:get_arch_storage_hive_starter/get_arch_storage_hive_starter.dart';
 
-class GetArchStorageHiveStarter
-    extends BaseConfigurableGetArchPackage<StorageConfig> {
+class GetArchStorageHiveStarter extends BasePackage<StorageConfig> {
   @override
   InitPackageDI? get initPackageDI => null;
 }
