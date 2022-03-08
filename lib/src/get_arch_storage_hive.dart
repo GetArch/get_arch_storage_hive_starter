12b8b52b7f8f28bb668b986de843306240b861c0
huwen <hu.wentao@outlook.com>
2022-03-08 14:55:02 +0800

    refactor get_arch_storage_hive.dart adapt BasePackage

--- lib/src/get_arch_storage_hive.dart
+++ lib/src/get_arch_storage_hive.dart
@@ -2,6 +2,18 @@ import 'package:get_arch_core/get_arch_core.dart';
 import 'package:get_arch_storage_hive_starter/get_arch_storage_hive_starter.dart';
 
 class GetArchStorageHiveStarter extends BasePackage<StorageConfig> {
-  @override
-  InitPackageDI? get initPackageDI => null;
+  GetArchStorageHiveStarter({
+    Future<void> Function(GetIt g, StorageConfig c)? onBeforePkgInit,
+    Future<void> Function(GetIt g, StorageConfig config)? onAfterPkgInit,
+    Future<void> Function(StorageConfig config, Object e, StackTrace s)?
+        onPkgInitError,
+    Future<void> Function(StorageConfig config)? onPkgInitFinally,
+    Future<void> Function(GetIt getIt, StorageConfig config)? onPackageInit,
+  }) : super(
+          onBeforePkgInit,
+          onAfterPkgInit,
+          onPkgInitError,
+          onPkgInitFinally,
+          onPackageInit,
+        );
 }
