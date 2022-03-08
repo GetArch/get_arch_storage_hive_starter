7048eff721e5d225dd7ad90d1373d767b7b2d41e
huwen <hu.wentao@outlook.com>
2022-03-08 15:32:22 +0800

    feat storage_config.dart 根据pubspec自动配置 Storage的name,version,packAt

--- lib/src/base/storage_config.dart
+++ lib/src/base/storage_config.dart
@@ -1,4 +1,6 @@
 import 'package:get_arch_core/get_arch_core.dart';
+import 'package:get_arch_storage_hive_starter/src/constants/pubspec.yaml.g.dart'
+    as spec;
 
 ///
 /// 存储信息配置
@@ -6,20 +8,16 @@ class StorageConfig extends BaseConfig {
   final String basePath;
   final String? key;
 
-  StorageConfig(this.basePath, this.key,
-      {required EnvSign sign,
-      required String name,
-      required String version,
-      required DateTime packAt})
-      : super(sign: sign, name: name, version: version, packAt: packAt);
-
-  // const StorageConfig({
-  //   required this.basePath,
-  //   this.key,
-  // });
-
-  @override
-  List<Object?> get props => [basePath, key];
+  StorageConfig({
+    required EnvSign sign,
+    required this.basePath,
+    this.key,
+  }) : super(
+          sign: sign,
+          name: spec.name,
+          version: spec.version,
+          packAt: DateTime.fromMillisecondsSinceEpoch(spec.timestamp * 1000),
+        );
 
   @override
   Map<String, dynamic> toJson() => {
